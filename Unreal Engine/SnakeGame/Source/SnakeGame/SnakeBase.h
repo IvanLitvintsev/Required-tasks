7be903fdb160774d3256b165e5983782132c6661
Denis Kovtun <denkov26@outlook.com>
2024-05-28 08:38:31 +0300

    Snake: add snake body
    logig of appearance

---
+++ Unreal Engine/SnakeGame/Source/SnakeGame/SnakeBase.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SnakeBase.generated.h"
+
+class ASnakeElementBase;
+
+UCLASS()
+class SNAKEGAME_API ASnakeBase : public AActor
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<ASnakeElementBase> SnakeElementClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	float ElementSize;
+
+	UPROPERTY()
+	TArray<ASnakeElementBase*> SnakeElements;
+public:	
+	// Sets default values for this actor's properties
+	ASnakeBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void AddSnakeElement(int ElementsNum = 1);
+
+};
